aa100991e26da0ea71bc9959b71905ff313bdee2
gihpuccini <gxp6079@g.rit.edu>
2020-03-21 15:44:31 -0400

    dropdown

--- app/src/main/java/com/example/myfoodalert/Activities/NewFoodTypeDialog.kt
+++ app/src/main/java/com/example/myfoodalert/Activities/NewFoodTypeDialog.kt
@@ -5,12 +5,16 @@ import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import com.example.myfoodalert.R
 
 
 
 class NewFoodTypeDialog : DialogFragment() {
+
+    val foodGroups = arrayOf("Carbohydrates", "Proteins", "Milk and dairy", "Fruits and vegetables", "Fats and sugars")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return  inflater.inflate(R.layout.add_to_food_db, container, false)
@@ -28,6 +32,13 @@ class NewFoodTypeDialog : DialogFragment() {
             // add to db
             this.dismiss()
         }
+
+        val spinner = view.findViewById<Spinner>(R.id.food_type_spinner)
+        val aa = ArrayAdapter(context, android.R.layout.simple_spinner_item, foodGroups)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = aa
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
